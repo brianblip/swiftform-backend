3f8da229c2ae5ab9449ba3f39e4d68e6761fb2a0
Renzo Visperas <renzovisperas07@gmail.com>
2023-12-24 17:22:39 +0800

    provide simple prompt for creating a form

--- app.py
+++ app.py
@@ -1,28 +1,47 @@
-from flask import Flask
+from flask import Flask, request, jsonify, Blueprint
 from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()  # load environment variables from .env.
-client = OpenAI()
 app = Flask(__name__)
 
-@app.route("/api/v1/prompt")
+
+@app.route("/api/v1/prompt", methods=["POST"])
 def prompt():
   try:
-    return {
-      'data': 'sassadsadasda'
-    }, 200
+    text = request.json.get('text')
+
+    return jsonify({
+      'data': get_completion(create_prompt(text))
+    })
   except Exception as e:
     print(e)
     return {
       'error': 'Uknown error occured. Please try again later.'
     }
-    # completion = client.chat.completions.create(
-    #   model="gpt-3.5-turbo",
-    #   messages=[
-    #     {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    #     {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-    #   ]
-    # )
+ 
+    
+client = OpenAI()
+    
+def get_completion(prompt, model="gpt-3.5-turbo"):
+  messages = [{"role": "user", "content": prompt}]
+  response =  client.chat.completions.create(
+    model=model,
+    messages=messages,
+    temperature=0
+  )
+  
+  return response.choices[0].message.content
 
-    # return f"<p>{completion.choices[0].message}</p>"
+def create_prompt(text):
+  prompt = f"""
+    You will be provided with text delimited by triple backticks.
+    Your task is to provide input fields for a form described in the text.
+    Provide an array of objects in json format with the following keys:
+    label, name, and type
+    
+    ```{text}```
+    """
+  
+  return prompt
+   
