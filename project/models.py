bb0285705afe93516d91ad942c62b72dd3e200ce
Renzo Visperas <renzoviperas07@gmail.com>
2024-01-12 09:42:22 +0800

    add user_identity_loader and user_lookup_loader

--- project/models.py
+++ project/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Enum
 
 from . import db
+from . import jwt
 
 class Session(db.Model):
     __tablename__ = 'sessions'
@@ -16,6 +17,21 @@ class User(db.Model):
     email = db.Column(db.Text, nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
     avatar_url = db.Column(db.Text, nullable=False)
+
+# Register a callback function that takes whatever object is passed in as the
+# identity when creating JWTs and converts it to a JSON serializable format.
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.id
+
+# Register a callback function that loads a user from your database whenever
+# a protected route is accessed. This should return any python object on a
+# successful lookup, or None if the lookup failed for any reason (for example
+# if the user has been deleted from the database).
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
 
 class Notification(db.Model):
     id = db.Column(db.Integer, primary_key=True)
