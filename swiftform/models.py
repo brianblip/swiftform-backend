93c9078b364bcca6b8415052c717a46d3a65aad5
Renzo Visperas <renzoviperas07@gmail.com>
2024-01-18 12:10:06 +0800

    Add User model

---
+++ swiftform/models.py
@@ -0,0 +1,8 @@
+from swiftform.app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text, nullable=False, unique=True)
+    password = db.Column(db.Text, nullable=False)
+    avatar_url = db.Column(db.Text, nullable=False)
