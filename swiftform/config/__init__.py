c0640d7b000caa49761345febe20212383fb8299
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-01-19 11:54:42 +0800

    Delete config/__init__.py
    We unify configuration to config.py

--- swiftform/config/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-SECRET_KEY = os.environ["SECRET_KEY"]
-DEBUG = os.environ.get("FLASK_DEBUG", "false").lower() in ("true", "1")
-
-SERVER_NAME = os.getenv(
-    "SERVER_NAME", "localhost:{0}".format(os.getenv("PORT", "8000"))
-)
-
