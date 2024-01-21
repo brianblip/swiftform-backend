3289496b50947aacc4e93e5a42eae540e7698a87
Renzo Visperas <renzoviperas07@gmail.com>
2024-01-21 17:20:42 +0800

    add user_identity_loader in models.py
    - Registers a callback function that takes whatever object is passed in as the identity when creating JWTs and converts it to a JSON serializable format.

--- swiftform/models.py
+++ swiftform/models.py
@@ -1,4 +1,4 @@
-from swiftform.app import db
+from swiftform.app import db, jwt
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -6,3 +6,10 @@ class User(db.Model):
     email = db.Column(db.Text, nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
     avatar_url = db.Column(db.Text, nullable=False)
+
+
+# Register a callback function that takes whatever object is passed in as the
+# identity when creating JWTs and converts it to a JSON serializable format.
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.id
