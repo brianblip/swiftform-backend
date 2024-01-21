e2cbe925168530b06b6dd7978327f8637f6cf062
Renzo Visperas <renzoviperas07@gmail.com>
2024-01-21 19:40:30 +0800

    create check if token is revoked in models.py
    Callback function to check if a JWT exists in the database blocklist

--- swiftform/models.py
+++ swiftform/models.py
@@ -35,3 +35,11 @@ class TokenBlocklist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     jti = db.Column(db.String(36), nullable=False, index=True)
     created_at = db.Column(db.DateTime, nullable=False)
+
+# Callback function to check if a JWT exists in the database blocklist
+@jwt.token_in_blocklist_loader
+def check_if_token_revoked(jwt_header, jwt_payload: dict) -> bool:
+    jti = jwt_payload["jti"]
+    token = db.session.query(TokenBlocklist.id).filter_by(jti=jti).scalar()
+
+    return token is not None
