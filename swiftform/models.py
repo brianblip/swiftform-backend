a54742c6058e8acd3863ce42821bac3b92ea184c
Renzo Visperas <renzoviperas07@gmail.com>
2024-01-21 18:19:56 +0800

    add user_lookup_loader
    Register a callback function that loads a user from your database whenever a protected route is accessed. This should return any python object on a successful lookup, or None if the lookup failed for any reason (for example if the user has been deleted from the database).

--- swiftform/models.py
+++ swiftform/models.py
@@ -13,3 +13,12 @@ class User(db.Model):
 @jwt.user_identity_loader
 def user_identity_lookup(user):
     return user.id
+
+# Register a callback function that loads a user from your database whenever
+# a protected route is accessed. This should return any python object on a
+# successful lookup, or None if the lookup failed for any reason (for example
+# if the user has been deleted from the database).
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
