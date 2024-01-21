276c053cf8d5d93348e78d911914607db93351a3
Renzo Visperas <renzoviperas07@gmail.com>
2024-01-21 16:33:06 +0800

    add SQLALCHEMY_DATABASE_URI in swiftform.config

--- swiftform/config.py
+++ swiftform/config.py
@@ -11,3 +11,16 @@ class Config(object):
     SERVER_NAME = os.getenv(
         "SERVER_NAME", "{0}:{1}".format(os.getenv("FLASK_HOST", "localhost"), os.getenv("FLASK_PORT", "8000"))
     )
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "SQLALCHEMY_DATABASE_URI",
+        "postgresql://{0}:{1}@{2}:{3}/{4}".
+        format(
+            os.getenv("POSTGRES_USER"),
+            os.getenv("POSTGRES_PASSWORD"),
+            os.getenv("POSTGRES_HOST"),
+            os.getenv("POSTGRES_PORT"),
+            os.getenv("POSTGRES_DB"),
+        ),
+    )
+
