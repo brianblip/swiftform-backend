f1b1d0c4227137b94a56d9ace5d9b0c7d6cc9b83
Renzo Visperas <renzoviperas07@gmail.com>
2024-01-27 10:08:51 +0800

    register prompt blueprint

--- swiftform/app.py
+++ swiftform/app.py
@@ -32,6 +32,9 @@ def create_app():
     from swiftform.api.users import users
     app.register_blueprint(users)
 
+    from swiftform.api.prompt import prompt
+    app.register_blueprint(prompt)
+
     app.register_error_handler(Exception, handle_exception)
     app.register_error_handler(400, handle_bad_request)
     app.register_error_handler(401, handle_unauthorized)
