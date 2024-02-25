22359ef7a2f7f1ea675b726f4d6da3099c5310b8
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-02-25 21:54:52 +0800

    Dynamically load all API routes inside the module

--- swiftform/api/__init__.py
+++ swiftform/api/__init__.py
@@ -1,6 +1,18 @@
+import importlib
+import os
+
 from flask import Blueprint
 
 api = Blueprint("api", __name__, url_prefix="/api/v1")
+
+
+# Import all API routes in this module
+api_routes = [f for f in os.listdir(os.path.dirname(
+    os.path.abspath(__file__))) if f.endswith(".py") and f != "__init__.py"]
+
+for api_route in api_routes:
+    importlib.import_module(os.path.dirname(
+        os.path.realpath(__file__)).split('/')[-1] + "." + api_route[:-3])
 
 
 @api.route("/")
