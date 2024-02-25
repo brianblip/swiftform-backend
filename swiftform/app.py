fc7e7a61348a3f64d98d7e4368ca65c86cd720e4
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-02-25 23:29:16 +0800

    Refresh JWT tokens validity in every request

--- swiftform/app.py
+++ swiftform/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from swiftform.config import Config
 from swiftform.database import db
-from swiftform.jwt_manager import jwt
+from swiftform.jwt_manager import (jwt, refresh_expiring_token)
 from flask_alembic import Alembic
 from flask_cors import CORS
 from swiftform.api import api
@@ -39,6 +39,8 @@ def create_app():
                                ExceptionHandlers.handle_not_found)
     app.register_error_handler(422,
                                ExceptionHandlers.handle_unprocessable_content)
+
+    app.after_request(refresh_expiring_token)
 
     return app
 
