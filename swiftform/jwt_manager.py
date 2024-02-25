1d59ff066434baf7b234ae77c7cfa9bb28f2f309
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-02-25 23:11:40 +0800

    Add JWT callbacks handling user identity and tokens
    This was previously removed in app.py and is introduced back

--- swiftform/jwt_manager.py
+++ swiftform/jwt_manager.py
@@ -29,3 +29,30 @@ def refresh_expiring_token(response):
     except (RuntimeError, KeyError):
         # Return original response even when JWT is invalid
         return response
+
+
+@jwt.token_in_blocklist_loader
+def is_token_revoked(jwt_header, jwt_payload: dict) -> bool:
+    """
+    Check if JWT exists in database
+    """
+    jti = jwt_payload["jti"]
+    token = db.session.query(TokenBlocklist.id).filter_by(jti=jti).scalar()
+
+    return token is not None
+
+
+@jwt.user_lookup_loader
+def user_lookup(_jwt_header, jwt_data):
+    """
+    Attaches a user object when accessing a protected route.
+
+    The user object may be None when is not found in database.
+    """
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
+
+
+@jwt.user_identity_loader
+def get_user_id(user):
+    return user.id
