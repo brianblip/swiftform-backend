37dd9e9d73f21962472b616495353aafa5d3789a
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-02-25 22:55:41 +0800

    Introduce method to refresh JWT tokens before they expire

--- swiftform/jwt_manager.py
+++ swiftform/jwt_manager.py
@@ -1,3 +1,31 @@
-from flask_jwt_extended import JWTManager
+import datetime
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    current_user,
+    get_jwt,
+    set_access_cookies,
+)
+from swiftform.database import db
+from swiftform.models import TokenBlocklist, User
 
 jwt = JWTManager()
+
+
+def refresh_expiring_token(response):
+    """
+    Reset JWT token validity with each request and refresh
+    token expiration within 30 minutes of consecutive requests.
+    """
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(datetime.timezone.utc)
+        target_timestamp = datetime.timestamp(
+            now + datetime.timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=current_user)
+            set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        # Return original response even when JWT is invalid
+        return response
