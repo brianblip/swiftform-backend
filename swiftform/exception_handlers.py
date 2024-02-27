60537666de88511ec464be271500cf3c21e54d64
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-02-27 08:19:58 +0800

    Create validation error handler and register to app

--- swiftform/exception_handlers.py
+++ swiftform/exception_handlers.py
@@ -1,8 +1,20 @@
 from flask import jsonify
+from swiftform.validation.validation import ValidationRuleErrors
+
+
+def on_validation_error(e: ValidationRuleErrors):
+    errors = []
+    for error in e.error_bags:
+        errors.append({"field": error.attribute, "message": error.description})
+
+    return jsonify({"message": errors[0]['message'], "errors": errors})
+
 
 def on_exception(e):
     return jsonify({"message": str(e)}), 500
 
+
 class ExceptionHandlers(object):
     def init_app(self, app):
         app.register_error_handler(Exception, on_exception)
+        app.register_error_handler(ValidationRuleErrors, on_validation_error)
