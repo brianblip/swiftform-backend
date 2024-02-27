19568bcbf3a3047d569afacdde9203090c0d7791
Jan Tolentino <tolentinojancarlo@gmail.com>
2024-02-27 08:31:33 +0800

    Add Required validation rule
    Accepts a single attribute, or an array of attribute names

---
+++ swiftform/validation/rules.py
@@ -0,0 +1,19 @@
+from swiftform.validation.validation import ValidationRule, ValidationRuleError
+from flask import request
+
+
+class Required(ValidationRule):
+    def __init__(self, attributes):
+        self.attributes = attributes
+
+    def validate(self) -> None:
+        attributes = self.attributes
+
+        if not isinstance(self.attributes, list):
+            attributes = [attributes]
+
+        for attribute in attributes:
+            if attribute not in request.json.keys():
+                raise ValidationRuleError(attribute, f"The {attribute} is required.")
+
+        pass
