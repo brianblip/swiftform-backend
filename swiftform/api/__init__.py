8d388b6981e8d39065e2dfb81c6747b15c8366af
Neo <lordneobarnachea@gmail.com>
2024-03-05 14:43:32 +0800

    add init

--- swiftform/api/__init__.py
+++ swiftform/api/__init__.py
@@ -6,13 +6,17 @@ from flask import Blueprint
 api = Blueprint("api", __name__, url_prefix="/api/v1")
 
 
-# Import all API routes in this module
-api_routes = [f for f in os.listdir(os.path.dirname(
-    os.path.abspath(__file__))) if f.endswith(".py") and f != "__init__.py"]
-
-for api_route in api_routes:
-    importlib.import_module(os.path.dirname(
-        os.path.realpath(__file__)).split('/')[-1] + "." + api_route[:-3])
+import swiftform.api.answer
+import swiftform.api.auth
+import swiftform.api.avatar_upload
+import swiftform.api.choice
+import swiftform.api.form
+import swiftform.api.notification
+import swiftform.api.prompt
+import swiftform.api.question
+import swiftform.api.response
+import swiftform.api.section
+import swiftform.api.users
 
 
 @api.route("/")
